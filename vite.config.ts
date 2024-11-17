59aeda73b48e62b295a6edf25e8475c24daf7d11
Aman Dewett <amandewett@gmail.com>
2024-11-17 17:00:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allows the app to be accessed by IP
+    port: 5174, // Optional: to ensure it runs on the correct port
+  },
+});
