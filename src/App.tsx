640c60d629ac9953984e296532ec22898dc973c4
Aman Dewett <contact@dewett.ca>
2025-08-08 18:53:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import CounterComponent from './components/counter/CounterComponent';
 
 const App: React.FC = () => {
-  return <CounterComponent eventTime={1750252800000} />;
+  return <CounterComponent eventTime={1760587200000} />;
 };
 
 export default App;
