2a57e85cfa6dea830e0297a609dbb5a2e2f66bc1
Aman Dewett <contact@dewett.ca>
2025-08-08 18:38:15 -0400

    date updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import CounterComponent from "./components/counter/CounterComponent";
+import CounterComponent from './components/counter/CounterComponent';
 
 const App: React.FC = () => {
-  return <CounterComponent eventTime={1761696000000} />;
+  return <CounterComponent eventTime={1750166400000} />;
 };
 
 export default App;
